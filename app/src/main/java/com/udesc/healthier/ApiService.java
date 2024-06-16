f2a4bcc62d84b527655043de4f5e546fe6dd2861
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2024-06-16 19:47:14 -0300

    feat: connection

---
+++ app/src/main/java/com/udesc/healthier/ApiService.java
@@ -0,0 +1,10 @@
+package com.udesc.healthier;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface ApiService {
+    @GET("/workout-plan")
+    Call<Object> getTeste();
+}
--- app/src/main/java/com/udesc/healthier/MainActivity.java
+++ app/src/main/java/com/udesc/healthier/MainActivity.java
@@ -1,14 +1,25 @@
 package com.udesc.healthier;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView responseTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,43 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        checkContactsReadPermission();
+
+        // Vincula o elemento da UI ao objeto Java
+        responseTextView = findViewById(R.id.responseTextView);
+
+        // Faz a requisição à API
+        makeApiRequest();
+    }
+
+    protected void checkContactsReadPermission() {
+        String permission = Manifest.permission.INTERNET;
+        int granted = PackageManager.PERMISSION_GRANTED;
+        if (ContextCompat.checkSelfPermission(this, permission) != granted) {
+            ActivityCompat.requestPermissions(this, new String[]{permission}, 0);
+        }
+    }
+
+    private void makeApiRequest() {
+        ApiService apiService = RetrofitClient.getClient().create(ApiService.class);
+        Call<Object> call = apiService.getTeste();
+
+        call.enqueue(new Callback<Object>() {
+            @Override
+            public void onResponse(Call<Object> call, Response<Object> response) {
+                if (response.isSuccessful()) {
+                    responseTextView.setText(response.body().toString());
+                } else {
+                    responseTextView.setText("Request failed: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Object> call, Throwable t) {
+                responseTextView.setText("Request failed: " + t.getMessage());
+            }
         });
     }
 }
