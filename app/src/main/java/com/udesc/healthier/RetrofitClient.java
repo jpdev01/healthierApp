a433b05905db965b6cde008a5a03dc95a0ca1d24
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2024-06-17 21:26:28 -0300

    feat: perform login

--- app/src/main/java/com/udesc/healthier/RetrofitClient.java
+++ app/src/main/java/com/udesc/healthier/RetrofitClient.java
@@ -1,5 +1,8 @@
 package com.udesc.healthier;
 
+import android.content.Context;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,10 +10,23 @@ public class RetrofitClient {
 
     private static final String BASE_URL = "http://192.168.0.2:8080";
     private static Retrofit retrofit = null;
+    private static String token;
+
+    public static void init(Context context) {
+        String authToken = new TokenManager(context).getToken();
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new AuthInterceptor(authToken))
+                .build();
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient) // Adicione o cliente HTTP com o interceptor
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+    }
 
     public static Retrofit getClient() {
         if (retrofit == null) {
-            retrofit = new Retrofit.Builder()
+            return new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
---
+++ app/src/main/java/com/udesc/healthier/TokenManager.java
@@ -0,0 +1,31 @@
+package com.udesc.healthier;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class TokenManager {
+    private static final String SHARED_PREFS_NAME = "my_shared_prefs";
+    private static final String KEY_JWT_TOKEN = "jwt_token";
+
+    private SharedPreferences sharedPreferences;
+
+    public TokenManager(Context context) {
+        this.sharedPreferences = context.getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE);
+    }
+
+    public void saveToken(String token) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(KEY_JWT_TOKEN, token);
+        editor.apply();
+    }
+
+    public String getToken() {
+        return sharedPreferences.getString(KEY_JWT_TOKEN, null);
+    }
+
+    public void clearToken() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.remove(KEY_JWT_TOKEN);
+        editor.apply();
+    }
+}
